8bd771e27eb583a3f33a56643b6194bc67adcdfb
Magnocube <stefangroenendijk1998@hotmail.com>
2021-05-09 13:34:10 +0200

    server: Added first functions.

--- gamestate.cpp
+++ gamestate.cpp
@@ -4,3 +4,15 @@ GameState::GameState()
 {
 
 }
+
+/*Makes a stream of data wich represents the gamestate */
+size_t GameState::serialize(QBitArray &buf)
+{
+
+}
+
+/*fills the gamestate with from the data received from server*/
+bool GameState::deSerialize(QBitArray &buf)
+{
+
+}
--- gamestate.h
+++ gamestate.h
@@ -1,11 +1,15 @@
 #ifndef GAMESTATE_H
 #define GAMESTATE_H
-
+#include <QBitArray>
 
 class GameState
 {
 public:
     GameState();
+    size_t serialize(QBitArray &buf);
+    bool deSerialize(QBitArray &buf);
+
+
 };
 
 #endif // GAMESTATE_H
