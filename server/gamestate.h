43b5e3f96cbba0eb3c5c697113130389ab2fecfa
Remco van Gorsel <r.gorsel98@upcmail.nl>
2021-05-15 17:43:59 +0200

    server: updated gamestate.h

--- server/gamestate.h
+++ server/gamestate.h
@@ -2,6 +2,7 @@
 #define GAMESTATE_H
 
 #include "QString"
+#include <QList>
 #include "stdint.h"
 
 enum class GameStatePartition{
@@ -10,11 +11,52 @@ enum class GameStatePartition{
     NOTHING
 };
 
+enum class Belt{
+    WHITE,
+    WHITEYELLOW,
+    YELLOW,
+    YELLOWGREEN,
+    GREEN,
+    GREENBLUE,
+    BLUE,
+    BLUERED,
+    RED,
+    REDBLACK,
+    BLACK,
+    BLACK2,
+    BLACK3,
+    BLACK4,
+    BLACK5,
+    BLACK6,
+    BLACK7,
+    BLACK8,
+    BLACK9,
+};
+
+struct PlayerData{
+    QString name;
+    QString middleName;
+    QString lastName;
+    int8_t age;
+    Belt beltDegree;
+    QString club;
+    //A variable for tracking scores
+    //A variable for how many rounds
+    //A variable for which Poomsae for which round
+};
+
 struct GameState{
     QString version;
-    QString matchName;
     QString creationDate;
-
+    QString matchName;
+    QString currentDivision; //eq 17 over 1 and 2 kup Male
+    int8_t numberOfJuries;
+    int8_t numberOfRounds; //Depends on number of players
+    int8_t timePerMatch;
+    QList<PlayerData> participatingPlayers;
+    QList<PlayerData> eliminatedPlayers;
+    PlayerData currentPlayer;
 };
+
 
 #endif // GAMESTATE_H
